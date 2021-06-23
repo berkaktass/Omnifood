4b94e9b5462648d26e842bdbafda14b90fe52ae2
Mert ULAS <h.mert.ulas@gmail.com>
2021-06-24 00:53:12 +0300

    date bug fixed

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,3 +13,6 @@ function makeSticky(){
         navbar.classList.remove("sticky");
     }
 }
+
+const footerDate = document.querySelector(".date");
+footerDate.innerHTML = `<p>Copyright &copy; ${ new Date().getFullYear() } by Omnifood. All rights reserved.</p>`;
